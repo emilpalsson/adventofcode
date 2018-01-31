597873476b8f9a6b6d370d28994ca4b955748508
emilpalsson <emil@emilpalsson.se>
2018-01-31 12:54:15 +0100

    Add 2016 day 8

---
+++ 2016/day08.js
@@ -0,0 +1,41 @@
+const { getInput, range } = require("./utils.js");
+const instructions = getInput(8, true).map(x => x.split(/[ =xyb]+/));
+
+const screen = range(6).map(i => range(50).fill(" "));
+
+const rect = (a, b) => {
+  range(b).forEach(bi => {
+    range(a).forEach(ai => {
+      screen[bi][ai] = "#";
+    });
+  });
+};
+
+const rotate = (arr, steps) => [...arr.slice(-steps), ...arr.slice(0, -steps)];
+
+const rotateColumn = (index, steps) => {
+  let column = screen.map(row => row[index]);
+  column = rotate(column, steps);
+  column.forEach((value, rowIndex) => (screen[rowIndex][index] = value));
+};
+
+const rotateRow = (index, steps) => {
+  screen[index] = rotate(screen[index], steps);
+};
+
+instructions.forEach(p => {
+  if (p[0] === "rect") {
+    rect(parseInt(p[1], 10), parseInt(p[2], 10));
+  } else if (p[1] === "column") {
+    rotateColumn(parseInt(p[2], 10), parseInt(p[3], 10));
+  } else {
+    rotateRow(parseInt(p[2], 10), parseInt(p[3], 10));
+  }
+});
+
+const activePixels = screen.reduce((sum, row) => {
+  return sum + row.filter(c => c === "#").length;
+}, 0);
+
+console.log("#1:", activePixels);
+console.log("#2:", "\n" + screen.map(row => row.join("")).join("\n"));
