e46e0ca47aca8a9fd720feb3072ddc6f6ca22886
emilpalsson <emil@emilpalsson.se>
2018-02-05 22:35:25 +0100

    Add 2016 day 12

---
+++ 2016/day12.js
@@ -0,0 +1,32 @@
+const { getInput, range } = require("./utils.js");
+const commands = getInput(12, true).map(x => x.split(" "));
+
+const day12 = registers => {
+  let pos = 0;
+
+  const val = x => (/\d+/.test(x) ? parseInt(x, 10) : registers[x]);
+  const cpy = (source, target) => (registers[target] = val(source));
+  const inc = register => registers[register]++;
+  const dec = register => registers[register]--;
+
+  const execute = cmd => {
+    switch (cmd[0]) {
+      case "cpy":
+        return cpy(cmd[1], cmd[2]);
+      case "inc":
+        return inc(cmd[1]);
+      case "dec":
+        return dec(cmd[1]);
+    }
+  };
+
+  while (pos >= 0 && pos < commands.length) {
+    const cmd = commands[pos];
+    execute(cmd);
+    pos += cmd[0] === "jnz" && val(cmd[1]) ? parseInt(cmd[2], 10) : 1;
+  }
+  return registers.a;
+};
+
+console.log("#1:", day12({ a: 0, b: 0, c: 0, d: 0 }));
+console.log("#2:", day12({ a: 0, b: 0, c: 1, d: 0 }));
