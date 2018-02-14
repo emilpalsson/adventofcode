e67e35e39251b887726d7b84ec7961615633a599
Emil Pålsson <emil@emilpalsson.se>
2018-02-14 23:34:28 +0100

    Add A* util

---
+++ utils/astar.js
@@ -0,0 +1,107 @@
+const getId = cords => `${cords[0]}x${cords[1]}`;
+
+const getHeuristicDistance = (from, to) =>
+  Math.abs(to[0] - from[0]) + Math.abs(to[1] - from[1]);
+
+const NodeModel = (
+  cords,
+  costSoFar = Infinity,
+  minimumTotalCost = Infinity
+) => ({
+  id: getId(cords),
+  cords,
+  cameFrom: null,
+  costSoFar,
+  minimumTotalCost,
+  closed: false
+});
+
+const astar = (start, goal, getNeighbors) => {
+  const nodes = {};
+  nodes[getId(start)] = NodeModel(start, 0, getHeuristicDistance(start, goal));
+  const openSet = [getId(start)];
+  const goalId = getId(goal);
+
+  // Find the node with minimum minimumTotalCost and set to current (to try it next)
+  let current;
+  const setNextCurrent = () => {
+    const sorted = openSet
+      .map(id => ({ id, minimumTotalCost: nodes[id].minimumTotalCost }))
+      .sort((a, b) => a.minimumTotalCost - b.minimumTotalCost);
+    if (sorted.length > 0) {
+      current = nodes[sorted[0].id];
+      return true;
+    }
+    return false;
+  };
+
+  while (setNextCurrent()) {
+    // Solution found?
+    if (current.id === goalId) {
+      return current.costSoFar;
+      break;
+    }
+
+    // Remove current node from openSet and set to closed
+    openSet.splice(openSet.indexOf(current.id), 1);
+    nodes[current.id].closed = true;
+
+    const neighbors = getNeighbors(current.cords)
+      // .filter(n => n[0] !== 0 || n[1] !== 2)
+      // .filter(n => n[0] !== 1 || n[1] !== 2)
+      // .filter(n => n[0] !== 2 || n[1] !== 2)
+      // .filter(n => n[0] !== 2 || n[1] !== 1)
+      // .filter(n => n[0] !== 4 || n[1] !== 0)
+      // .filter(n => n[0] !== 4 || n[1] !== 1)
+      // .filter(n => n[0] !== 4 || n[1] !== 2)
+      // .filter(n => n[0] !== 4 || n[1] !== 3)
+      // .filter(n => n[0] !== 4 || n[1] !== 4)
+      // .filter(n => n[0] !== 3 || n[1] !== 4)
+      // .filter(n => n[0] !== 2 || n[1] !== 4)
+      // .filter(n => n[0] !== 1 || n[1] !== 4)
+      // .filter(n => n[0] !== 1 || n[1] !== 5)
+      // .filter(n => n[0] !== 1 || n[1] !== 6)
+      .map(n => NodeModel(n));
+
+    neighbors.forEach(neighbor => {
+      if (!nodes[neighbor.id]) {
+        // New node found, add to node list and openSet
+        nodes[neighbor.id] = neighbor;
+        openSet.push(neighbor.id);
+      } else if (nodes[neighbor.id].closed) {
+        // Node already closed, continue
+        return;
+      }
+
+      // Ignore path if not better than previously found
+      const costSoFar = nodes[current.id].costSoFar + 1;
+      if (costSoFar >= nodes[neighbor.id].costSoFar) {
+        return;
+      }
+
+      // Best path found to neighbor so far, save it
+      nodes[neighbor.id].cameFrom = current.id;
+      nodes[neighbor.id].costSoFar = costSoFar;
+      nodes[neighbor.id].minimumTotalCost =
+        costSoFar + getHeuristicDistance(nodes[neighbor.id].cords, goal);
+    });
+  }
+
+  // Unsolvable
+  return false;
+};
+
+const cost = astar(
+  [1, 1],
+  [2, 5],
+  (getNeighbors = pos =>
+    pos[0] >= 0 && pos[0] <= 50 && pos[1] >= 0 && pos[1] <= 50
+      ? [
+          [pos[0], pos[1] - 1],
+          [pos[0] + 1, pos[1]],
+          [pos[0], pos[1] + 1],
+          [pos[0] - 1, pos[1]]
+        ]
+      : [])
+);
+console.log(cost);
