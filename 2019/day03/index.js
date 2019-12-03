5890438430dcfe45fe905cfe188fcb193bf6114d
emilpalsson <emil@emilpalsson.se>
2019-12-03 22:12:25 +0100

    Refactor 2019 day 3

--- 2019/day03/index.js
+++ 2019/day03/index.js
@@ -1,153 +1,52 @@
 const { getInput } = require("../../utils");
-const input = getInput(true, false).map(wire =>
-  wire.split(",").map(move => ({
-    direction: move.substr(0, 1),
-    length: Number(move.substr(1))
+const input = getInput(true).map(wire =>
+  wire.split(",").map(line => ({
+    direction: line.substr(0, 1),
+    length: Number(line.substr(1))
   }))
 );
 
-const wire1 = input[0]; //.map(getXYFromeMove);
-const wire2 = input[1];
-const pos = { x: 0, y: 0 };
+const step = (state, direction) => {
+  state.steps++;
+  if (direction === "R") state.x++;
+  else if (direction === "L") state.x--;
+  else if (direction === "D") state.y++;
+  else if (direction === "U") state.y--;
+  return `${state.x};${state.y}`;
+};
 
-const id = () => `${pos.x};${pos.y}`;
+const plotWire1 = () => {
+  const map = {};
+  const state = { x: 0, y: 0, steps: 0 };
 
-let minTotal = Infinity;
+  input[0].forEach(line => {
+    for (let i = 0; i < line.length; i++) {
+      const coords = step(state, line.direction);
+      map[coords] = map[coords] || state.steps;
+    }
+  });
+  return map;
+};
+const wire1 = plotWire1();
 
-let steps = 0;
-const map = {};
-const plotRight = (wire, length) => {
-  for (let i = 0; i < length; i++) {
-    steps++;
-    pos.x++;
-    const currId = id();
-    if (wire === 1) {
-      if (!map[currId]) {
-        map[currId] = steps;
-      }
-    } else {
-      if (map[currId]) {
-        console.log("INTERSECTION", pos, getDistance(), map[currId] + steps);
-        const totalSteps = map[currId] + steps;
-        if (totalSteps < minTotal) {
-          minTotal = totalSteps;
-        }
+const findClosestIntersections = () => {
+  let distance = Infinity;
+  let steps = Infinity;
+  const state = { x: 0, y: 0, steps: 0 };
+
+  input[1].forEach(line => {
+    for (let i = 0; i < line.length; i++) {
+      const coords = step(state, line.direction);
+      if (wire1[coords]) {
+        distance = Math.min(distance, Math.abs(state.x) + Math.abs(state.y));
+        steps = Math.min(steps, wire1[coords] + state.steps);
       }
     }
-  }
+  });
+
+  return { distance, steps };
 };
-const plotLeft = (wire, length) => {
-  for (let i = 0; i < length; i++) {
-    steps++;
-    pos.x--;
-    const currId = id();
-    if (wire === 1) {
-      if (!map[currId]) {
-        map[currId] = steps;
-      }
-    } else {
-      if (map[currId]) {
-        console.log("INTERSECTION", pos, getDistance(), map[currId] + steps);
-        const totalSteps = map[currId] + steps;
-        if (totalSteps < minTotal) {
-          minTotal = totalSteps;
-        }
-      }
-    }
-  }
-};
-const plotDown = (wire, length) => {
-  for (let i = 0; i < length; i++) {
-    steps++;
-    pos.y++;
-    const currId = id();
-    if (wire === 1) {
-      if (!map[currId]) {
-        map[currId] = steps;
-      }
-    } else {
-      if (map[currId]) {
-        console.log("INTERSECTION", pos, getDistance(), map[currId] + steps);
-        const totalSteps = map[currId] + steps;
-        if (totalSteps < minTotal) {
-          minTotal = totalSteps;
-        }
-      }
-    }
-  }
-};
-const plotUp = (wire, length) => {
-  for (let i = 0; i < length; i++) {
-    steps++;
-    pos.y--;
-    const currId = id();
-    if (wire === 1) {
-      if (!map[currId]) {
-        map[currId] = steps;
-      }
-    } else {
-      if (map[currId]) {
-        console.log("INTERSECTION", pos, getDistance(), map[currId] + steps);
-        const totalSteps = map[currId] + steps;
-        if (totalSteps < minTotal) {
-          minTotal = totalSteps;
-        }
-      }
-    }
-  }
-};
+const result = findClosestIntersections();
 
-const intersections = [];
-const getDistance = () => {
-  const distance = Math.abs(pos.x) + Math.abs(pos.y);
-  intersections.push(distance);
-  return distance;
-};
-
-wire1.forEach(move => {
-  switch (move.direction) {
-    case "R":
-      plotRight(1, move.length);
-      break;
-    case "L":
-      plotLeft(1, move.length);
-      break;
-    case "D":
-      plotDown(1, move.length);
-      break;
-    case "U":
-      plotUp(1, move.length);
-      break;
-  }
-});
-
-pos.x = 0;
-pos.y = 0;
-steps = 0;
-
-wire2.forEach(move => {
-  switch (move.direction) {
-    case "R":
-      plotRight(2, move.length);
-      break;
-    case "L":
-      plotLeft(2, move.length);
-      break;
-    case "D":
-      plotDown(2, move.length);
-      break;
-    case "U":
-      plotUp(2, move.length);
-      break;
-  }
-});
-
-intersections.sort();
-console.log(intersections);
-
-minTotal;
-
-// console.log(Object.keys(map));
-
-// console.log("#1:", part1()); // 3562672
-// console.log("#2:", part2()); // 8250
+console.log("#1:", result.distance); // 5357
+console.log("#2:", result.steps); // 101956
