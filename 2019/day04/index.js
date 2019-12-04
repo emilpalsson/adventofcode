2ea097164f40701e17be51be6df239b4f2c433f4
emilpalsson <emil@emilpalsson.se>
2019-12-04 06:29:52 +0100

    Add 2019 day 4 first working part 2

--- 2019/day04/index.js
+++ 2019/day04/index.js
@@ -5,11 +5,21 @@ const input = getInput()
 
 const [from, to] = input;
 
-const regex = /11|22|33|44|55|66|77|88|99|00/;
-
 let combinations = 0;
 for (let i = from; i <= to; i++) {
-  if (!regex.test(i.toString())) {
+  if (
+    !(
+      (i.toString().includes("11") && !i.toString().includes("111")) ||
+      (i.toString().includes("22") && !i.toString().includes("222")) ||
+      (i.toString().includes("33") && !i.toString().includes("333")) ||
+      (i.toString().includes("44") && !i.toString().includes("444")) ||
+      (i.toString().includes("55") && !i.toString().includes("555")) ||
+      (i.toString().includes("66") && !i.toString().includes("666")) ||
+      (i.toString().includes("77") && !i.toString().includes("777")) ||
+      (i.toString().includes("88") && !i.toString().includes("888")) ||
+      (i.toString().includes("99") && !i.toString().includes("999"))
+    )
+  ) {
     continue;
   }
   if (
@@ -24,4 +34,4 @@ for (let i = from; i <= to; i++) {
   combinations++;
 }
 
-combinations;
+combinations; // 511, 316
