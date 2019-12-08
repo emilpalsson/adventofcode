17c509926cb5e426f9493ba25645eaa716670e3b
emilpalsson <emil@emilpalsson.se>
2019-12-08 09:35:47 +0100

    Add 2019 day 8 first working part 2

--- 2019/day08/index.js
+++ 2019/day08/index.js
@@ -1,30 +1,71 @@
 const { getInput } = require("../../utils");
 const input = getInput();
 
-const layerSize = 25 * 6;
-const layerCount = input.length / layerSize;
+const IMAGE_WIDTH = 25;
+const IMAGE_HEIGHT = 6;
+const COLOR = {
+  BLACK: 0,
+  WHITE: 1,
+  TRANSPARENT: 2
+};
 
-const layers = [];
-for (let i = 0; i < layerCount; i++) {
-  layers.push(input.substr(i * layerSize, layerSize));
-}
+const layerSize = IMAGE_WIDTH * IMAGE_HEIGHT;
 
-const getDigitCount = (str, digit) => str.match(new RegExp(digit, "g")).length;
-
-let layerWithFewest0;
-let layerWithFewest0Count = Infinity;
-layers.forEach(layer => {
-  const layer0Count = getDigitCount(layer, 0);
-  if (layer0Count < layerWithFewest0Count) {
-    layerWithFewest0Count = layer0Count;
-    layerWithFewest0 = layer;
+const chop = (str, chunkSize) => {
+  const chunkCount = str.length / chunkSize;
+  const chunks = [];
+  for (let i = 0; i < chunkCount; i++) {
+    chunks.push(str.substr(i * chunkSize, chunkSize));
   }
-});
+  return chunks;
+};
 
-const oneCount = getDigitCount(layerWithFewest0, 1);
-const twoCount = getDigitCount(layerWithFewest0, 2);
+const layers = chop(input, layerSize);
 
-console.log(oneCount * twoCount); // 2520
+const part1 = () => {
+  const getDigitCount = (str, digit) =>
+    (str.match(new RegExp(digit, "g")) || []).length;
+
+  let layerWithFewest0;
+  let layerWithFewest0Count = Infinity;
+  layers.forEach(layer => {
+    const layer0Count = getDigitCount(layer, 0);
+    if (layer0Count < layerWithFewest0Count) {
+      layerWithFewest0Count = layer0Count;
+      layerWithFewest0 = layer;
+    }
+  });
+
+  const oneCount = getDigitCount(layerWithFewest0, 1);
+  const twoCount = getDigitCount(layerWithFewest0, 2);
+
+  console.log(oneCount * twoCount); // 2520
+};
+
+const part2 = () => {
+  layers.reverse();
+
+  const finalPixels = layers[0].split("").map(Number);
+
+  layers.forEach(layer => {
+    layer
+      .split("")
+      .map(Number)
+      .forEach((pixel, index) => {
+        if (pixel === COLOR.BLACK || pixel === COLOR.WHITE) {
+          finalPixels[index] = pixel;
+        }
+      });
+  });
+
+  const finalRows = chop(finalPixels.join(""), IMAGE_WIDTH);
+  const image = finalRows
+    .map(row => row.replace(new RegExp(COLOR.BLACK, "g"), " "))
+    .join("\n");
+  console.log(image);
+};
+
+part2();
 
 // console.log("#1:", part1());
 // console.log("#2:", part2());
