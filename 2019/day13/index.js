481afb319d6f07d6f0f5d6456cfb2544695e6e7b
emilpalsson <emil@emilpalsson.se>
2019-12-13 20:16:26 +0100

    Refactor 19:13

--- 2019/day13/index.js
+++ 2019/day13/index.js
@@ -4,35 +4,10 @@ const input = getInput(false, false)
   .split(",")
   .map(Number);
 
-input[0] = 2;
-
-const DIRECTION = {
-  UP: 0,
-  RIGHT: 1,
-  DOWN: 2,
-  LEFT: 3
-};
-
-const COLOR = {
-  BLACK: 0,
-  WHITE: 1
-};
-
-const TURN = {
-  LEFT: 0,
-  RIGHT: 1
-};
-
 const OUTPUT_MODE = {
   X_POS: 0,
   Y_POS: 1,
   TILE_ID: 2
-};
-
-const JOYSTICK_POS = {
-  NEUTRAL: 0,
-  LEFT: -1,
-  RIGHT: 1
 };
 
 const TILE_TYPE = {
@@ -43,29 +18,56 @@ const TILE_TYPE = {
   BALL: 4
 };
 
-const emergencyHullPaintingRobot = () => {
-  const panels = {};
+const JOYSTICK_POS = {
+  NEUTRAL: 0,
+  LEFT: -1,
+  RIGHT: 1
+};
+
+const part1 = () => {
+  const tiles = {};
   let currentOutputMode = OUTPUT_MODE.X_POS;
   let x;
   let y;
 
   const getId = () => `${x},${y}`;
 
+  const onOutput = output => {
+    switch (currentOutputMode) {
+      case OUTPUT_MODE.X_POS:
+        x = output;
+        break;
+      case OUTPUT_MODE.Y_POS:
+        y = output;
+        break;
+      case OUTPUT_MODE.TILE_ID:
+        tiles[getId()] = output;
+        break;
+    }
+    currentOutputMode = (currentOutputMode + 1) % 3;
+  };
+
+  const computer = intcodeComputer({ program: input, onOutput });
+  computer.run();
+
+  return Object.values(tiles).filter(x => x === TILE_TYPE.BLOCK).length;
+};
+
+const part2 = () => {
+  const tiles = {};
+  let currentOutputMode = OUTPUT_MODE.X_POS;
+  let x;
+  let y;
+  let points = 0;
+  let ballX;
+  let paddleX;
+
+  const getId = () => `${x},${y}`;
+
   const onInput = () => {
-    // return panels[getId()] || COLOR.BLACK;
-    const entries = Object.entries(panels);
-    const ballPos = entries
-      .find(e => e[1] === TILE_TYPE.BALL)[0]
-      .split(",")
-      .map(Number);
-    const paddlePos = entries
-      .find(e => e[1] === TILE_TYPE.PADDLE)[0]
-      .split(",")
-      .map(Number);
-    // console.log(ballPos, paddlePos);
-    if (ballPos[0] < paddlePos[0]) {
+    if (ballX < paddleX) {
       return JOYSTICK_POS.LEFT;
-    } else if (ballPos[0] > paddlePos[0]) {
+    } else if (ballX > paddleX) {
       return JOYSTICK_POS.RIGHT;
     }
     return JOYSTICK_POS.NEUTRAL;
@@ -73,60 +75,31 @@ const emergencyHullPaintingRobot = () => {
 
   const onOutput = output => {
     if (currentOutputMode === OUTPUT_MODE.X_POS) {
-      // panels[getId()] = output;
-      // console.log("x", output);
       x = output;
-      currentOutputMode = OUTPUT_MODE.Y_POS;
     } else if (currentOutputMode === OUTPUT_MODE.Y_POS) {
-      // console.log("y", output);
       y = output;
-      // const turnValue = output === TURN.LEFT ? 3 : 1;
-      // robotPos.direction = (robotPos.direction + turnValue) % 4;
-      // // prettier-ignore
-      // switch (robotPos.direction) {
-      //   case DIRECTION.UP: robotPos.y--; break;
-      //   case DIRECTION.DOWN: robotPos.y++; break;
-      //   case DIRECTION.LEFT: robotPos.x--; break;
-      //   case DIRECTION.RIGHT: robotPos.x++; break;
-      // }
-      currentOutputMode = OUTPUT_MODE.TILE_ID;
     } else {
-      // console.log("tileid", output);
       if (x === -1 && y === 0) {
-        // SCORE
-        console.log(output);
+        points = output;
       } else {
-        panels[getId()] = output;
+        tiles[getId()] = output;
+        if (output === TILE_TYPE.BALL) {
+          ballX = x;
+        } else if (output === TILE_TYPE.PADDLE) {
+          paddleX = x;
+        }
       }
-      currentOutputMode = OUTPUT_MODE.X_POS;
     }
+    currentOutputMode = (currentOutputMode + 1) % 3;
   };
 
-  const computer = intcodeComputer({ program: input, onInput, onOutput });
+  const program = input.slice();
+  program[0] = 2;
+  const computer = intcodeComputer({ program, onInput, onOutput });
   computer.run();
 
-  return panels;
+  return points;
 };
 
-const part1 = () => {
-  const panels = emergencyHullPaintingRobot();
-  // return Object.values(panels).filter(x => x === 2).length;
-  return panels;
-};
-
-// const part2 = () => {
-//   const panels = emergencyHullPaintingRobot(COLOR.WHITE);
-//   const s = Array(50).fill(" ");
-//   const rows = [];
-//   for (let i = 0; i < 6; i++) {
-//     rows.push(s.slice());
-//   }
-//   Object.entries(panels).forEach(([pos, color]) => {
-//     const apa = pos.split(",").map(Number);
-//     rows[apa[1]][apa[0]] = color === COLOR.BLACK ? " " : "#";
-//   });
-//   return rows.map(r => r.join("")).join("\n");
-// };
-
-console.log("#1:", part1()); // 1885
-// console.log("#2:\n" + part2()); // BFEAGHAF
+console.log("#1:", part1()); // 270
+console.log("#2:", part2()); // 12535
