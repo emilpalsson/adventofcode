2f46d5d3c9c42088065bec7efbd6c703227acaef
emilpalsson <emil@emilpalsson.se>
2019-12-16 10:57:24 +0100

    19:16 - Simplify part 1 a bit

--- 2019/day16/index.js
+++ 2019/day16/index.js
@@ -3,48 +3,31 @@ const input = getInput()
   .split("")
   .map(Number);
 
-const basePattern = [0, 1, 0, -1];
-
-input;
+const pattern = [0, 1, 0, -1];
+const patternLength = pattern.length;
 
 let values = input.slice();
 
-const getPattern = index => {
-  const pattern = [];
-  basePattern.forEach(multiplier => {
-    for (let i = 0; i <= index; i++) {
-      pattern.push(multiplier);
-    }
-  });
-  return pattern;
-  // return pattern.slice(1);
-};
-
-// getPattern(1);
-// console.log(getPattern(1));
+const valuesLength = values.length;
 
 const performPhase = () => {
-  values = values.map((value1, index1) => {
-    const pattern = getPattern(index1);
+  for (let i = 0; i < valuesLength; i++) {
     let newValue = 0;
-    values.forEach((value2, index2) => {
-      // if (index1 === 0) {
-      const multiplier = pattern[(index2 + 1) % pattern.length];
-      // console.log(value2, multiplier);
-      newValue += value2 * multiplier;
-      // 1*1  + 2*0  + 3*-1 + 4*0  + 5*1  + 6*0  + 7*-1 + 8*0
-      // 1*0  + 2*1  + 3*1  + 4*0  + 5*0  + 6*-1 + 7*-1 + 8*0
-      // }
+    values.forEach((value, i2) => {
+      const multiplier =
+        pattern[Math.floor((i2 + 1) / (i + 1)) % patternLength];
+      newValue += value * multiplier;
     });
-    const newValueStr = Math.abs(newValue).toString();
-    return Number(newValueStr.substr(newValueStr.length - 1));
-  });
+    values[i] = Math.abs(newValue) % 10;
+  }
 };
 
 for (let i = 0; i < 100; i++) {
   performPhase();
 }
-console.log(values.join("").substr(0, 8));
+
+const answer = values.join("").substr(0, 8);
+console.log(answer, answer === "42205986"); // 42205986
 
 // console.log("#1:", answer.part1); // 258
 // console.log("#2:", answer.part2); // 372
