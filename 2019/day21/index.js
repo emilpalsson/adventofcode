175282da3d6f1a820ffac2583b060809d37fcba0
emilpalsson <emil@emilpalsson.se>
2019-12-21 09:04:07 +0100

    19:21 - Refactor

--- 2019/day21/index.js
+++ 2019/day21/index.js
@@ -4,90 +4,52 @@ const input = getInput()
   .split(",")
   .map(Number);
 
-const part1 = () => {
-  let outputLine = "";
+const part1Input = [
+  "NOT A J",
+  "NOT B T",
+  "OR T J",
+  "NOT C T",
+  "OR T J",
+  "NOT D T",
+  "NOT T T",
+  "AND T J",
+  "WALK",
+  ""
+].join("\n");
 
+const part2Input = [
+  "NOT A J",
+  "NOT B T",
+  "OR T J",
+  "NOT C T",
+  "OR T J",
+  "NOT D T",
+  "NOT T T",
+  "AND T J",
+  "NOT I T",
+  "NOT T T",
+  "OR F T",
+  "AND E T",
+  "OR H T",
+  "AND T J",
+  "RUN",
+  ""
+].join("\n");
+
+const getHullDamage = inputCommand => {
+  let hullDamage;
   let inputCommandPointer = 0;
-  const inputCommand = [
-    "NOT A J",
-    "NOT B T",
-    "OR T J",
-    "NOT C T",
-    "OR T J",
-    "NOT D T",
-    "NOT T T",
-    "AND T J",
-    "WALK",
-    ""
-  ].join("\n");
 
-  const onInput = () => {
-    return inputCommand.charCodeAt(inputCommandPointer++);
-  };
+  const onInput = () => inputCommand.charCodeAt(inputCommandPointer++);
 
   const onOutput = output => {
-    if (output === 10) {
-      console.log(outputLine);
-      outputLine = "";
-    } else {
-      if (output > 255) {
-        console.log(output);
-      } else {
-        outputLine += String.fromCharCode(output);
-      }
-    }
+    hullDamage = output;
   };
 
   const computer = intcodeComputer({ program: input, onInput, onOutput });
   computer.run();
+  return hullDamage;
 };
 
-const part2 = () => {
-  let outputLine = "";
-
-  let inputCommandPointer = 0;
-  const inputCommand = [
-    "NOT A J",
-    "NOT B T",
-    "OR T J",
-    "NOT C T",
-    "OR T J",
-    "NOT D T",
-    "NOT T T",
-    "AND T J",
-    "NOT I T",
-    "NOT T T",
-    "OR F T",
-    "AND E T",
-    "OR H T",
-    "AND T J",
-    "RUN",
-    ""
-  ].join("\n");
-
-  const onInput = () => {
-    return inputCommand.charCodeAt(inputCommandPointer++);
-  };
-
-  const onOutput = output => {
-    if (output === 10) {
-      console.log(outputLine);
-      outputLine = "";
-    } else {
-      if (output > 255) {
-        console.log(output);
-      } else {
-        outputLine += String.fromCharCode(output);
-      }
-    }
-  };
-
-  const computer = intcodeComputer({ program: input, onInput, onOutput });
-  computer.run();
-};
-
-// part1();
-part2();
-
-// console.log("#1:", answer.part1); // 258
-// console.log("#2:", answer.part2); // 372
+console.log("#1:", getHullDamage(part1Input)); // 19358870
+console.log("#2:", getHullDamage(part2Input)); // 1143356492
