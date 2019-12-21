3021a69a3e4c615131d1810427e7b50490d1777d
emilpalsson <emil@emilpalsson.se>
2019-12-21 08:54:36 +0100

    19:21 - First working part 2

--- 2019/day21/index.js
+++ 2019/day21/index.js
@@ -4,7 +4,7 @@ const input = getInput()
   .split(",")
   .map(Number);
 
-const solve = () => {
+const part1 = () => {
   let outputLine = "";
 
   let inputCommandPointer = 0;
@@ -20,17 +20,6 @@ const solve = () => {
     "WALK",
     ""
   ].join("\n");
-
-  // A||B||C = hole, D = ground
-  // NOT A J
-  // NOT B T
-  // OR T J
-  // NOT C T
-  // OR T J
-  // NOT D T
-  // NOT T T
-  // AND T J
-  // WALK
 
   const onInput = () => {
     return inputCommand.charCodeAt(inputCommandPointer++);
@@ -53,6 +42,52 @@ const solve = () => {
   computer.run();
 };
 
-const answer = solve();
+const part2 = () => {
+  let outputLine = "";
+
+  let inputCommandPointer = 0;
+  const inputCommand = [
+    "NOT A J",
+    "NOT B T",
+    "OR T J",
+    "NOT C T",
+    "OR T J",
+    "NOT D T",
+    "NOT T T",
+    "AND T J",
+    "NOT I T",
+    "NOT T T",
+    "OR F T",
+    "AND E T",
+    "OR H T",
+    "AND T J",
+    "RUN",
+    ""
+  ].join("\n");
+
+  const onInput = () => {
+    return inputCommand.charCodeAt(inputCommandPointer++);
+  };
+
+  const onOutput = output => {
+    if (output === 10) {
+      console.log(outputLine);
+      outputLine = "";
+    } else {
+      if (output > 255) {
+        console.log(output);
+      } else {
+        outputLine += String.fromCharCode(output);
+      }
+    }
+  };
+
+  const computer = intcodeComputer({ program: input, onInput, onOutput });
+  computer.run();
+};
+
+// part1();
+part2();
+
 // console.log("#1:", answer.part1); // 258
 // console.log("#2:", answer.part2); // 372
