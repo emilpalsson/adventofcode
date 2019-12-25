2337f946592ad65a3f00606725fcd629cef1887a
emilpalsson <emil@emilpalsson.se>
2019-12-25 15:47:24 +0100

    19:22 - Calculate instead of array fiddling for part 1

--- 2019/day22/index.js
+++ 2019/day22/index.js
@@ -1,46 +1,35 @@
-const { getInput, range } = require("../../utils");
+const { getInput } = require("../../utils");
 const input = getInput(true);
 
-let deck = range(10007);
+const CARD_COUNT = 10007;
+let position = 2019;
 
 const dealIntoNewStack = () => {
-  deck.reverse();
+  position = CARD_COUNT - position - 1;
 };
 
 const dealWithIncrement = increment => {
-  let pointer = 0;
-  const newDeck = [];
-  deck.forEach(card => {
-    newDeck[pointer] = card;
-    pointer = (pointer + increment) % deck.length;
-  });
-  deck = newDeck;
+  position = increment * position;
 };
 
 const cut = num => {
-  if (num > 0) {
-    deck = [...deck.slice(num), ...deck.slice(0, num)];
-  } else {
-    deck = [...deck.slice(num), ...deck.slice(0, num)];
-  }
+  position = position - num;
 };
 
 input.forEach(step => {
   if (step === "deal into new stack") {
-    console.log("deal into new stack");
     dealIntoNewStack();
-  } else if (step.startsWith("deal with increment ")) {
+  } else if (step.startsWith("deal with")) {
     const increment = Number(step.substr(20));
-    console.log("deal with increment", increment);
     dealWithIncrement(increment);
   } else if (step.startsWith("cut ")) {
     const num = Number(step.substr(4));
-    console.log("cut", num);
     cut(num);
   }
+  position = position % CARD_COUNT;
 });
 
-console.log(deck.indexOf(2019));
+console.log(position); // 4775
 
 // console.log("#1:", part1()); // 32526865
 // console.log("#2:", part2()); // 2009
