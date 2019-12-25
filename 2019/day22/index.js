1b45aa05cebf9923d8053f56358f443e0f4ae18c
emilpalsson <emil@emilpalsson.se>
2019-12-25 16:10:24 +0100

    19:22 - Move to BigInt

--- 2019/day22/index.js
+++ 2019/day22/index.js
@@ -1,26 +1,18 @@
 const { getInput } = require("../../utils");
 const input = getInput(true);
 
-// const dealIntoNewStack = (currentPos, cardCount) => cardCount - currentPos - 1;
-// const dealWithIncrement = (currentPos, cardCount, x) => x * currentPos;
-// const cut = (currentPos, cardCount, x) => currentPos - x;
-
-// const dealIntoNewStack = () => (currentPos, cardCount) => cardCount - currentPos - 1;
-// const dealWithIncrement = increment => (currentPos) => increment * currentPos;
-// const cut = num => (currentPos) => currentPos - num;
-
 const steps = input.map(step => {
   if (step === "deal into new stack") {
-    return (currentPos, cardCount) => cardCount - currentPos - 1;
+    return (currentPos, cardCount) => cardCount - currentPos - 1n;
   } else if (step.startsWith("deal with")) {
-    return currentPos => Number(step.substr(20)) * currentPos;
+    return currentPos => BigInt(step.substr(20)) * currentPos;
   }
-  return currentPos => currentPos - Number(step.substr(4));
+  return currentPos => currentPos - BigInt(step.substr(4));
 });
 
 const part1 = () => {
-  const CARD_COUNT = 10007;
-  let position = 2019;
+  const CARD_COUNT = 10007n;
+  let position = 2019n;
   steps.forEach(step => {
     position = step(position, CARD_COUNT) % CARD_COUNT;
   });
