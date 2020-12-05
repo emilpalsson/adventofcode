8a1e9d1c987b961ad6f7b48bea24a282b345c4e8
emilpalsson <emil@emilpalsson.se>
2020-12-05 19:23:15 +0100

    20:04 - Refactor

--- 2020/day04/index.js
+++ 2020/day04/index.js
@@ -1,63 +1,44 @@
 const { getInput } = require("../../utils");
-const input = getInput(true, false);
-
-const passports = [];
-
-const parse = () => {
-  let passport = {};
-  input.forEach((line) => {
-    if (line.trim() === "") {
-      passports.push(passport);
-      passport = {};
-    } else {
-      line.split(" ").forEach((param) => {
-        const [attr, value] = param.split(":");
-        passport[attr] = value;
-      });
-    }
+const requiredProps = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+const input = getInput()
+  .replace(/\r/g, "")
+  .split("\n\n")
+  .map((str) =>
+    str.split(/\s/).reduce((passport, param) => {
+      const [key, value] = param.split(":");
+      passport[key] = value;
+      return passport;
+    }, {})
+  )
+  .filter((passport) => {
+    return !requiredProps.some((prop) => !passport.hasOwnProperty(prop));
   });
-  passports.push(passport);
-};
-parse();
-// passports;
 
-const part1 = () => {
-  const validPassports = passports.filter((passport) => {
-    if (!passport.hasOwnProperty("byr")) return false;
-    if (!passport.hasOwnProperty("iyr")) return false;
-    if (!passport.hasOwnProperty("eyr")) return false;
-    if (!passport.hasOwnProperty("hgt")) return false;
-    if (!passport.hasOwnProperty("hcl")) return false;
-    if (!passport.hasOwnProperty("ecl")) return false;
-    if (!passport.hasOwnProperty("pid")) return false;
-    return true;
-  });
-  console.log(validPassports);
-  return validPassports;
-};
+const isBetweenInclusive = (value, min, max) => value >= min && value <= max;
+
+const part1 = () => input.length;
 
 const part2 = () => {
-  const validPassports = part1().filter((passport) => {
-    if (parseInt(passport.byr, 10) < 1920 || parseInt(passport.byr, 10) > 2002) return false;
-    if (parseInt(passport.iyr, 10) < 2010 || parseInt(passport.iyr, 10) > 2020) return false;
-    if (parseInt(passport.eyr, 10) < 2020 || parseInt(passport.eyr, 10) > 2030) return false;
-    if (passport.hgt && passport.hgt.endsWith("cm")) {
-      const hgt = parseInt(passport.hgt.replace("cm", ""), 10);
-      if (hgt < 150 || hgt > 193) return false;
+  const validPassports = input.filter((passport) => {
+    if (!isBetweenInclusive(parseInt(passport.byr, 10), 1920, 2002)) return false;
+    if (!isBetweenInclusive(parseInt(passport.iyr, 10), 2010, 2020)) return false;
+    if (!isBetweenInclusive(parseInt(passport.eyr, 10), 2020, 2030)) return false;
+
+    if (passport.hgt.endsWith("cm")) {
+      if (!isBetweenInclusive(parseInt(passport.hgt, 10), 150, 193)) return false;
     } else if (passport.hgt && passport.hgt.endsWith("in")) {
-      const hgt = parseInt(passport.hgt.replace("in", ""), 10);
-      if (hgt < 59 || hgt > 76) return false;
+      if (!isBetweenInclusive(parseInt(passport.hgt, 10), 59, 76)) return false;
     } else {
       return false;
     }
-    if (!/^#[0-9a-f]{6}$/.test(passport.hcl)) return false;
+
     if (!["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(passport.ecl)) return false;
+    if (!/^#[0-9a-f]{6}$/.test(passport.hcl)) return false;
     if (!/^[0-9]{9}$/.test(passport.pid)) return false;
     return true;
   });
-  console.log(validPassports);
   return validPassports.length;
 };
 
-// console.log("#1:", part1()); // 252 INTE 229
-console.log("#2:", part2()); // INTE 165
+console.log("#1:", part1()); // 230
+console.log("#2:", part2()); // 156
