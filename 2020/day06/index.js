c1a19bfc6e8f241ddc4d7355345627871d2e51e5
emilpalsson <emil@emilpalsson.se>
2020-12-06 15:15:21 +0100

    20:06 - Refactor

--- 2020/day06/index.js
+++ 2020/day06/index.js
@@ -1,29 +1,25 @@
 const { getInput } = require("../../utils");
-const input = getInput(false, false)
+const input = getInput()
   .replace(/\r/g, "")
   .split("\n\n")
-  .map((group) => {
-    return group.split("\n");
-  });
+  .map((group) => group.split("\n"));
 
-const countQuestionsWhereEveryoneAnsweredYes = (yesPerPerson) => {
-  let answeredYes = [...yesPerPerson[0]];
-
-  for (let i = yesPerPerson.length - 1; i > 0; i--) {
-    answeredYes.forEach((question) => {
-      if (!yesPerPerson[i].includes(question)) {
-        answeredYes = answeredYes.filter((x) => x !== question);
-      }
-    });
-  }
-
-  return answeredYes.length;
+const part1 = () => {
+  return input.reduce((sum, group) => {
+    const yesByAnyInGroup = new Set(group.join("").split("")).size;
+    return sum + yesByAnyInGroup;
+  }, 0);
 };
 
 const part2 = () => {
-  const apa = input.map(countQuestionsWhereEveryoneAnsweredYes);
-  return apa.reduce((sum, x) => sum + x, 0);
+  return input.reduce((sum, group) => {
+    let yesByAllInGroup = [...group[0]];
+    for (let i = group.length - 1; i > 0; i--) {
+      yesByAllInGroup = yesByAllInGroup.filter((q) => group[i].includes(q));
+    }
+    return sum + yesByAllInGroup.length;
+  }, 0);
 };
 
-// console.log("#1:", part1()); // 6703
+console.log("#1:", part1()); // 6703
 console.log("#2:", part2()); // 3430
