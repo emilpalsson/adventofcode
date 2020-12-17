7348f8f2ec25104396a8047bde9810458e38b5bc
emilpalsson <emil@emilpalsson.se>
2020-12-17 23:43:17 +0100

    20:13 - First working part 2

--- 2020/day13/index.js
+++ 2020/day13/index.js
@@ -3,29 +3,53 @@ const input = getInput(true);
 
 const part1 = () => {
   const earliestTimestamp = Number(input[0]);
-  const busses = input[1]
+  const buses = input[1]
     .split(",")
     .map(Number)
     .filter((x) => !Number.isNaN(x));
 
-  const bestBussChoice = busses.reduce(
-    (result, buss) => {
-      const roundTrips = Math.ceil(earliestTimestamp / buss);
-      const waitTime = roundTrips * buss - earliestTimestamp;
+  const bestBusChoice = buses.reduce(
+    (result, bus) => {
+      const roundTrips = Math.ceil(earliestTimestamp / bus);
+      const waitTime = roundTrips * bus - earliestTimestamp;
       if (waitTime < result.waitTime) {
-        return { buss, waitTime };
+        return { bus, waitTime };
       }
       return result;
     },
     { waitTime: Infinity }
   );
 
-  return bestBussChoice.buss * bestBussChoice.waitTime;
+  return bestBusChoice.bus * bestBusChoice.waitTime;
 };
 
 const part2 = () => {
-  return 0;
+  const buses = input[1]
+    .split(",")
+    .map((x, i) => ({ id: Number(x), offset: i }))
+    .filter((x) => !Number.isNaN(x.id));
+
+  const busWithLongestRoute = buses.reduce((result, bus) => {
+    return bus.id > result.id ? bus : result;
+  }, buses[0]);
+
+  const otherBuses = buses.filter((bus) => bus.id !== busWithLongestRoute.id);
+
+  let timestamp = busWithLongestRoute.id - busWithLongestRoute.offset;
+
+  const testTimestamp = () => {
+    return !otherBuses.some((bus) => !Number.isInteger((timestamp + bus.offset) / bus.id));
+  };
+
+  while (true) {
+    if (testTimestamp()) {
+      break;
+    }
+    timestamp += busWithLongestRoute.id;
+  }
+
+  return timestamp;
 };
 
 console.log("#1:", part1()); // 203
-// console.log("#2:", part2());
+console.log("#2:", part2()); // 905694340256752 (lol took several hours)
