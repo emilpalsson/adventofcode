323fcc6f50e7d3852e67df21462fcf7623f50449
emilpalsson <emil@emilpalsson.se>
2020-12-18 10:18:03 +0100

    20:18 - First working part 2

--- 2020/day18/index.js
+++ 2020/day18/index.js
@@ -2,6 +2,7 @@ const { getInput } = require("../../utils");
 const input = getInput(true);
 
 const innermostParenthesesExpressionRegex = /\([^()]+\)/;
+const additionOperationRegex = /\d+ \+ \d+/;
 
 const getFirstOperation = (expression) => {
   return expression.split(" ", 3).join(" ");
@@ -16,24 +17,35 @@ const calculateLeftToRight = (expression) => {
   return Number(expression);
 };
 
-const evaluate = (expression) => {
+const calculateWithPrecedence = (expression) => {
+  let match;
+  while ((match = additionOperationRegex.exec(expression))) {
+    const result = eval(match[0]);
+    const beforeOperation = expression.substr(0, match.index);
+    const afterOperation = expression.substr(match.index + match[0].length);
+    expression = beforeOperation + result + afterOperation;
+  }
+  return Number(eval(expression));
+};
+
+const evaluate = (expression, calculate) => {
   let match;
   while ((match = innermostParenthesesExpressionRegex.exec(expression))) {
-    const result = calculateLeftToRight(match[0].substr(1, match[0].length - 2));
+    const result = calculate(match[0].substr(1, match[0].length - 2));
     const beforeParentheses = expression.substr(0, match.index);
     const afterParentheses = expression.substr(match.index + match[0].length);
     expression = beforeParentheses + result + afterParentheses;
   }
-  return calculateLeftToRight(expression);
+  return calculate(expression);
 };
 
 const part1 = () => {
-  return input.reduce((sum, current) => sum + evaluate(current), 0);
+  return input.reduce((sum, current) => sum + evaluate(current, calculateLeftToRight), 0);
 };
 
 const part2 = () => {
-  return 0;
+  return input.reduce((sum, current) => sum + evaluate(current, calculateWithPrecedence), 0);
 };
 
 console.log("#1:", part1()); // 13976444272545
-// console.log("#2:", part2()); //
+console.log("#2:", part2()); // 88500956630893
