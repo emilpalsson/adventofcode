77c7d6a66abaf7a3ba8ca8a75189efb06a31cd17
emilpalsson <emil@emilpalsson.se>
2020-12-19 23:54:53 +0100

    20:19 - First working part 2

--- 2020/day19/index.js
+++ 2020/day19/index.js
@@ -1,31 +1,55 @@
 const { getInput } = require("../../utils");
-const input = getInput(false, false).replace(/\r/g, "");
+const input = getInput().replace(/\r/g, "");
 
-const replaceMatch = (string, match, replacement) =>
-  string.substr(0, match.index) + replacement + string.substr(match.index + match[0].length);
-
-const part1 = () => {
+const parseInput = () => {
   const [rulesSection, messages] = input.split("\n\n").map((x) => x.split("\n"));
   let rules = rulesSection.reduce((result, current) => {
     const parts = current.split(": ");
     result[Number(parts[0])] = parts[1].replace(/"/g, "");
     return result;
   }, []);
+  return { rules, messages };
+};
 
-  let match;
-  while ((match = /\d+/.exec(rules[0]))) {
-    let toInject = rules[Number(match[0])];
-    if (toInject.includes("|")) toInject = `(${toInject})`;
-    rules[0] = replaceMatch(rules[0], match, toInject);
-  }
-  const re = new RegExp(`^(${rules[0].replace(/ /g, "")})$`);
+const replaceMatch = (string, match, replacement) =>
+  string.substr(0, match.index) + replacement + string.substr(match.index + match[0].length);
 
+const solve = (rules, messages) => {
+  const ruleToRegex = (rule) => {
+    let match;
+    while ((match = /\d+/.exec(rule))) {
+      let toInject = rules[Number(match[0])];
+      if (toInject.includes("|")) toInject = `(${toInject})`;
+      rule = replaceMatch(rule, match, toInject);
+    }
+    return new RegExp(`^(${rule.replace(/ /g, "")})$`);
+  };
+
+  const re = ruleToRegex(rules[0]);
   return messages.filter((message) => re.test(message)).length;
 };
 
+const part1 = () => {
+  const { rules, messages } = parseInput();
+  return solve(rules, messages);
+};
+
 const part2 = () => {
-  return 0;
+  const { rules, messages } = parseInput();
+
+  rules[8] = "42 | 42 8";
+  rules[11] = "42 31 | 42 11 31";
+
+  const expandLoopRuleThreeTimes = (index) => {
+    for (let i = 0; i < 3; i++) rules[index] = rules[index].replace(index, `(${rules[index]})`);
+    rules[index] = rules[index].replace(index, "hopefully-not-deeper-lol");
+  };
+
+  expandLoopRuleThreeTimes(8);
+  expandLoopRuleThreeTimes(11);
+
+  return solve(rules, messages);
 };
 
 console.log("#1:", part1()); // 111
-// console.log("#2:", part2()); //
+console.log("#2:", part2()); // 343
