1357d3dcf1f24539c09796ff0b5ca8a91e219c75
emilpalsson <emil@emilpalsson.se>
2020-12-25 09:11:50 +0100

    20:22 - First working part 2

--- 2020/day22/index.js
+++ 2020/day22/index.js
@@ -3,6 +3,12 @@ const input = getInput().replace(/\r/g, "");
 
 const parseDecks = () => input.split("\n\n").map((x) => x.split("\n").slice(1).map(Number));
 
+const calculateScore = (deck) => {
+  return deck.reverse().reduce((result, card, index) => {
+    return result + card * (index + 1);
+  }, 0);
+};
+
 const part1 = () => {
   const [player1, player2] = parseDecks();
 
@@ -18,12 +24,6 @@ const part1 = () => {
     }
   };
 
-  const calculateScore = (deck) => {
-    return deck.reverse().reduce((result, card, index) => {
-      return result + card * (index + 1);
-    }, 0);
-  };
-
   while (player1.length > 0 && player2.length > 0) {
     play();
   }
@@ -32,8 +32,60 @@ const part1 = () => {
 };
 
 const part2 = () => {
-  return 0;
+  let winningDeck;
+
+  const play = (deck1, deck2) => {
+    const player1 = [...deck1];
+    const player2 = [...deck2];
+
+    const player1History = new Set();
+    const player2History = new Set();
+
+    while (true) {
+      // Check history
+      const player1State = player1.join("");
+      const player2State = player2.join("");
+      if (player2History.has(player2State)) {
+        // State already seen, player 1 wins
+        return 1;
+      }
+      player1History.add(player1State);
+      player2History.add(player2State);
+
+      // Draw cards
+      const p1 = player1.shift();
+      const p2 = player2.shift();
+
+      let winner;
+      if (player1.length >= p1 && player2.length >= p2) {
+        // Recurse (if enough cards)
+        winner = play(player1.slice(0, p1), player2.slice(0, p2));
+      } else {
+        // Highest card wins (if not enough cards to recurse)
+        winner = p1 > p2 ? 1 : 2;
+      }
+
+      // Apply result
+      if (winner === 1) {
+        player1.push(p1, p2);
+      } else {
+        player2.push(p2, p1);
+      }
+
+      // Check for winner
+      if (player1.length === 0) {
+        winningDeck = player2;
+        return 2;
+      } else if (player2.length === 0) {
+        winningDeck = player1;
+        return 1;
+      }
+    }
+  };
+
+  play(...parseDecks());
+  return calculateScore(winningDeck);
 };
 
 console.log("#1:", part1()); // 34566
-// console.log("#2:", part2()); //
+console.log("#2:", part2()); // 31854
