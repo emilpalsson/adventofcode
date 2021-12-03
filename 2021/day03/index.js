0da3b0cb5b9ff2b3c6487e2685eb1144177f335c
emilpalsson <emil@emilpalsson.se>
2021-12-03 21:40:44 +0100

    21:03 - First working part 2

--- 2021/day03/index.js
+++ 2021/day03/index.js
@@ -1,13 +1,13 @@
 const { getInput } = require("../../utils");
 const input = getInput(true);
 
-const threshold = input.length / 2;
-
 const part1 = () => {
-  const gammaRateDigits = input[0].split("").map((_, index) => {
-    return input.map((line) => line[index]).filter((bit) => bit === "1").length > threshold ? 1 : 0;
+  const threshold = input.length / 2;
+
+  const gammaRateDigits = input[0].split("").map((_, i) => {
+    return input.map((l) => l[i]).filter((b) => b === "1").length > threshold ? 1 : 0;
   });
-  const epsilonRateDigits = gammaRateDigits.map((bit) => (bit === 1 ? 0 : 1));
+  const epsilonRateDigits = gammaRateDigits.map((b) => (b === 1 ? 0 : 1));
 
   const gammaRate = parseInt(parseInt(gammaRateDigits.join(""), 10), 2);
   const epsilonRate = parseInt(parseInt(epsilonRateDigits.join(""), 10), 2);
@@ -16,8 +16,29 @@ const part1 = () => {
 };
 
 const part2 = () => {
-  return 0;
+  const lineLength = input[0].length;
+
+  let filteredLines = input;
+  for (let i = 0; i < lineLength; i++) {
+    const threshold = filteredLines.length / 2;
+    const mostCommonBit =
+      filteredLines.map((l) => l[i]).filter((b) => b === "1").length >= threshold ? "1" : "0";
+    filteredLines = filteredLines.filter((l) => l[i] === mostCommonBit);
+  }
+  const oxygenGeneratorRating = parseInt(parseInt(filteredLines[0], 10), 2);
+
+  filteredLines = input;
+  for (let i = 0; i < lineLength; i++) {
+    const threshold = filteredLines.length / 2;
+    const mostCommonBit =
+      filteredLines.map((l) => l[i]).filter((b) => b === "1").length >= threshold ? "1" : "0";
+    filteredLines = filteredLines.filter((l) => l[i] !== mostCommonBit);
+    if (filteredLines.length === 1) break;
+  }
+  const co2ScrubberRating = parseInt(parseInt(filteredLines[0], 10), 2);
+
+  return oxygenGeneratorRating * co2ScrubberRating;
 };
 
 console.log("#1:", part1()); // 841526
-// console.log("#2:", part2());
+console.log("#2:", part2()); // 4790390
