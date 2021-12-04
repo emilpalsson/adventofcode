36677ac4e3d607215f142bc640e7208e4a8ca56a
emilpalsson <emil@emilpalsson.se>
2021-12-04 22:35:25 +0100

    21:04 - First working part 1

---
+++ 2021/day04/index.js
@@ -0,0 +1,74 @@
+const { getInput } = require("../../utils");
+const input = getInput(true);
+
+const parseInput = () => {
+  const inputCopy = input.slice();
+  const numbers = inputCopy.shift().split(",").map(Number);
+  inputCopy.shift();
+
+  const parseNextBoard = () => {
+    const rows = [];
+    for (let i = 0; i < 5; i++) {
+      rows.push(
+        inputCopy
+          .shift()
+          .trim()
+          .split(/\s+/)
+          .map(Number)
+          .map((n) => ({ n }))
+      );
+    }
+    inputCopy.shift();
+
+    const scoreLines = [...rows];
+    for (let i = 0; i < 5; i++) {
+      scoreLines.push([rows[0][i], rows[1][i], rows[2][i], rows[3][i], rows[4][i]]);
+    }
+
+    return { rows, scoreLines };
+  };
+
+  const boards = [];
+  while (inputCopy.length > 0) {
+    boards.push(parseNextBoard());
+  }
+
+  return { numbers, boards };
+};
+
+const hasBingo = (board) => board.scoreLines.some((l) => l.every((b) => b.x));
+
+const countUnmarked = (board) =>
+  board.rows.reduce((sum, row) => {
+    row.filter((b) => !b.x).forEach((b) => (sum += b.n));
+    return sum;
+  }, 0);
+
+const part1 = () => {
+  const { numbers, boards } = parseInput();
+
+  let numberIndex = 0;
+  while (true) {
+    const number = numbers[numberIndex];
+    numberIndex++;
+
+    for (let bi = 0; bi < boards.length; bi++) {
+      const board = boards[bi];
+      board.rows.forEach((row) => {
+        row.filter((box) => box.n === number).forEach((box) => (box.x = 1));
+      });
+
+      if (hasBingo(board)) {
+        winner = board;
+        return countUnmarked(board) * number;
+      }
+    }
+  }
+};
+
+const part2 = () => {
+  return 0;
+};
+
+console.log("#1:", part1()); // 252
+// console.log("#2:", part2()); // 2608962048
