619a24cb55870ffab59ea578efcf40de63036e78
emilpalsson <emil@emilpalsson.se>
2021-12-07 20:48:43 +0100

    21:06 - First working part 2

--- 2021/day06/index.js
+++ 2021/day06/index.js
@@ -6,7 +6,6 @@ const part1 = () => {
 
   const tick = () => {
     const fishCount = fishList.length;
-
     for (let i = 0; i < fishCount; i++) {
       let nextAge = fishList[i] - 1;
       if (nextAge === -1) {
@@ -15,18 +14,29 @@ const part1 = () => {
       }
       fishList[i] = nextAge;
     }
-
-    // console.log(fishList.join(","));
   };
 
   for (let i = 0; i < 80; i++) tick();
-
   return fishList.length;
 };
 
 const part2 = () => {
-  return 0;
+  const fishCountBySpawnTimer = [];
+
+  for (let i = 0; i < 9; i++) {
+    fishCountBySpawnTimer[i] = input.filter((x) => x === i).length;
+  }
+
+  const tick = () => {
+    const fishReadyToSpawn = fishCountBySpawnTimer.shift();
+    fishCountBySpawnTimer[6] += fishReadyToSpawn;
+    fishCountBySpawnTimer[8] = fishReadyToSpawn;
+  };
+
+  for (let i = 0; i < 256; i++) tick();
+
+  return fishCountBySpawnTimer.reduce((sum, curr) => sum + curr, 0);
 };
 
 console.log("#1:", part1()); // 353274
-// console.log("#2:", part2());
+console.log("#2:", part2()); // 1609314870967
