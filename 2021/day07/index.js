2b68365114d879301b6dc0ee5730d559c8820f85
emilpalsson <emil@emilpalsson.se>
2021-12-07 21:18:02 +0100

    21:07 - First working part 2

--- 2021/day07/index.js
+++ 2021/day07/index.js
@@ -1,27 +1,44 @@
 const { getInput } = require("../../utils");
-const input = getInput(false, false).split(",").map(Number);
-
-const calculateFuelRequirement = (crabPositions, moveToPosition) => {
-  return crabPositions.reduce((sum, crabPosition) => {
-    return sum + Math.abs(moveToPosition - crabPosition);
-  }, 0);
-};
+const input = getInput(false).split(",").map(Number);
 
 const part1 = () => {
+  const calculateFuelRequirement = (moveToPosition) => {
+    return input.reduce((sum, crabPosition) => {
+      return sum + Math.abs(moveToPosition - crabPosition);
+    }, 0);
+  };
+
   const minPos = Math.min(...input);
   const maxPos = Math.max(...input);
 
   let minFuelRequirement = Infinity;
   for (let i = minPos; i <= maxPos; i++) {
-    minFuelRequirement = Math.min(minFuelRequirement, calculateFuelRequirement(input, i));
+    minFuelRequirement = Math.min(minFuelRequirement, calculateFuelRequirement(i));
   }
 
   return minFuelRequirement;
 };
 
 const part2 = () => {
-  return 0;
+  const calculateSingleMoveFuel = (steps) => (Math.pow(steps, 2) + steps) / 2;
+
+  const calculateFuelRequirement = (moveToPosition) => {
+    return input.reduce((sum, crabPosition) => {
+      const steps = Math.abs(moveToPosition - crabPosition);
+      return sum + calculateSingleMoveFuel(steps);
+    }, 0);
+  };
+
+  const minPos = Math.min(...input);
+  const maxPos = Math.max(...input);
+
+  let minFuelRequirement = Infinity;
+  for (let i = minPos; i <= maxPos; i++) {
+    minFuelRequirement = Math.min(minFuelRequirement, calculateFuelRequirement(i));
+  }
+
+  return minFuelRequirement;
 };
 
-console.log("#1:", part1()); // 0
-// console.log("#2:", part2()); // 0
+// console.log("#1:", part1()); // 359648
+console.log("#2:", part2()); // 0
