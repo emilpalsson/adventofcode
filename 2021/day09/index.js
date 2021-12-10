140eba8de72af952ba3159a77020647ea0af5eaf
emilpalsson <emil@emilpalsson.se>
2021-12-11 00:19:48 +0100

    21:09 - First working part 2

--- 2021/day09/index.js
+++ 2021/day09/index.js
@@ -4,32 +4,62 @@ const input = getInput(true).map((line) => line.split("").map(Number));
 const mapWidth = input[0].length;
 const mapHeight = input.length;
 
-const hasLowerNeighbour = (x, y) => {
-  const height = input[y][x];
-  if (y > 0 && input[y - 1][x] <= height) return true;
-  if (y < mapHeight - 1 && input[y + 1][x] <= height) return true;
-  if (x > 0 && input[y][x - 1] <= height) return true;
-  if (x < mapWidth - 1 && input[y][x + 1] <= height) return true;
-  return false;
+const getNeighbours = (x, y) => {
+  const neighbours = [];
+  if (y > 0) neighbours.push({ x, y: y - 1 });
+  if (y < mapHeight - 1) neighbours.push({ x, y: y + 1 });
+  if (x > 0) neighbours.push({ x: x - 1, y });
+  if (x < mapWidth - 1) neighbours.push({ x: x + 1, y });
+  return neighbours;
 };
 
-const part1 = () => {
-  let riskLevel = 0;
+const hasLowerNeighbour = (x, y) => {
+  return getNeighbours(x, y).some((point) => input[point.y][point.x] <= input[y][x]);
+};
 
+const findLowestPoints = () => {
+  const lowestPoints = [];
   for (let x = 0; x < mapWidth; x++) {
     for (let y = 0; y < mapHeight; y++) {
       if (!hasLowerNeighbour(x, y)) {
-        riskLevel += input[y][x] + 1;
+        lowestPoints.push({ x, y });
       }
     }
   }
+  return lowestPoints;
+};
 
-  return riskLevel;
+const getPointId = ({ x, y }) => `${x},${y}`;
+
+const countBasinSize = (lowestPoint) => {
+  const seenPoints = new Set();
+
+  const traverse = (x, y) => {
+    seenPoints.add(getPointId({ x, y }));
+    getNeighbours(x, y)
+      .filter((point) => input[point.y][point.x] < 9)
+      .filter((point) => !seenPoints.has(getPointId(point)))
+      .forEach((point) => {
+        traverse(point.x, point.y);
+      });
+  };
+  traverse(lowestPoint.x, lowestPoint.y);
+
+  return seenPoints.size;
+};
+
+const part1 = () => {
+  return findLowestPoints().reduce((sum, point) => sum + input[point.y][point.x] + 1, 0);
 };
 
 const part2 = () => {
-  return 0;
+  const biggestBasinSizes = findLowestPoints()
+    .map(countBasinSize)
+    .sort((a, b) => b - a)
+    .slice(0, 3);
+
+  return biggestBasinSizes[0] * biggestBasinSizes[1] * biggestBasinSizes[2];
 };
 
 console.log("#1:", part1()); // 452
-// console.log("#2:", part2()); // 0
+console.log("#2:", part2()); // 1263735
