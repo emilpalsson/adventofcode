3d0dbbfd25b6cf88908a9e244e10c4048950eecc
emilpalsson <emil@emilpalsson.se>
2021-12-23 14:40:32 +0100

    21:12 - First working part 2

--- 2021/day12/index.js
+++ 2021/day12/index.js
@@ -38,8 +38,31 @@ const part1 = () => {
 };
 
 const part2 = () => {
-  return 0;
+  let pathCount = 0;
+
+  const move = (caveId, path = []) => {
+    if (caveId === "start" && path.length > 0) return;
+
+    if (
+      caveId === caveId.toLowerCase() &&
+      path.includes(caveId) &&
+      path.some((x) => x === x.toLowerCase() && path.filter((y) => y === x).length > 1)
+    )
+      return;
+
+    if (caveId === "end") {
+      pathCount++;
+      return;
+    }
+
+    map[caveId].forEach((nextCaveId) => {
+      move(nextCaveId, [...path, caveId]);
+    });
+  };
+  move("start");
+
+  return pathCount;
 };
 
 console.log("#1:", part1()); // 4011
-// console.log("#2:", part2()); // 0
+console.log("#2:", part2()); // 108035
