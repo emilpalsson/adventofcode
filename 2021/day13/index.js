ca4d47adf0f1537b6f4f132e80b57d633f95f444
emilpalsson <emil@emilpalsson.se>
2021-12-26 00:09:25 +0100

    21:13 - Refactor

--- 2021/day13/index.js
+++ 2021/day13/index.js
@@ -1,18 +1,19 @@
 const { getInput } = require("../../utils");
 const input = getInput(false).replace(/\r\n/g, "\n");
 
-let [initialPoints, folds] = input.split("\n\n");
-initialPoints = initialPoints
-  .split("\n")
-  .map((x) => x.split(","))
-  .map(([x, y]) => ({ x: Number(x), y: Number(y) }));
-folds = folds
-  .split("\n")
-  .map((x) => x.split(/[ =]/))
-  .map(([_1, _2, direction, position]) => ({ direction, position }));
+const parseInput = () => {
+  let [points, folds] = input.split("\n\n");
+  points = points
+    .split("\n")
+    .map((p) => p.split(","))
+    .map((p) => ({ x: Number(p[0]), y: Number(p[1]) }));
+  folds = folds
+    .split("\n")
+    .map((f) => f.split(/[ =]/))
+    .map((f) => ({ direction: f[2], position: f[3] }));
+  return { points, folds };
+};
 
-const getMaxX = (points) => Math.max(...points.map(({ x }) => x));
-const getMaxY = (points) => Math.max(...points.map(({ y }) => y));
 const hasDot = (points, x, y) => points.some((p) => p.x === x && p.y === y);
 
 const fold = (points, direction, position) => {
@@ -20,41 +21,30 @@ const fold = (points, direction, position) => {
   belowTheFold.forEach((p) => (p[direction] = position * 2 - p[direction]));
 };
 
-const countUnique = (points) => new Set(points.map(({ x, y }) => `${x},${y}`)).size;
-
-const print = (points) => {
-  const maxY = getMaxY(points);
-  const maxX = getMaxX(points);
-
-  const apa = [];
-  for (let y = 0; y <= maxY; y++) {
-    apa[y] = [];
-    for (let x = 0; x <= maxX; x++) {
-      apa[y][x] = hasDot(points, x, y) ? "#" : " ";
-    }
-  }
-
-  console.log(apa.map((y) => y.join("")).join("\n"));
-};
-
 const part1 = () => {
-  fold(initialPoints, folds[0].direction, folds[0].position);
-
-  // print(initialPoints);
-  // fold(initialPoints, "y", 7);
-  // print(initialPoints);
-  // fold(initialPoints, "x", 5);
-  // print(initialPoints);
-
-  return countUnique(initialPoints);
+  const { points, folds } = parseInput();
+  fold(points, folds[0].direction, folds[0].position);
+  const uniqueDots = new Set(points.map(({ x, y }) => `${x},${y}`));
+  return uniqueDots.size;
 };
 
 const part2 = () => {
-  folds.forEach((x) => fold(initialPoints, x.direction, x.position));
-  print(initialPoints);
+  const { points, folds } = parseInput();
+  folds.forEach((x) => fold(points, x.direction, x.position));
 
-  return 0;
+  const sizeY = Math.max(...points.map(({ y }) => y));
+  const sizeX = Math.max(...points.map(({ x }) => x));
+
+  const code = [];
+  for (let y = 0; y <= sizeY; y++) {
+    code[y] = [];
+    for (let x = 0; x <= sizeX; x++) {
+      code[y][x] = hasDot(points, x, y) ? "#" : " ";
+    }
+  }
+
+  return code.map((y) => y.join("")).join("\n");
 };
 
-// console.log("#1:", part1()); // 618
-console.log("#2:", part2()); // ALREKFKU
+console.log("#1:", part1()); // 618
+console.log(`#2:\n${part2()}`); // ALREKFKU
