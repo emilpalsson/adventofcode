395eb53b0bc5b373688f112d7d8fd79b98f930d1
emilpalsson <emil@emilpalsson.se>
2021-12-27 23:50:19 +0100

    21:15 - First working part 2

--- 2021/day15/index.js
+++ 2021/day15/index.js
@@ -2,9 +2,10 @@ const { getInput } = require("../../utils");
 const { astar } = require("./astar");
 const input = getInput(true).map((line) => line.split("").map(Number));
 
-const bottomRight = [input[0].length - 1, input.length - 1];
-
 const part1 = () => {
+  const map = input;
+  const bottomRight = [map[0].length - 1, map.length - 1];
+
   const getNeighbors = ([x, y]) => {
     return [
       [x, y - 1], // up
@@ -14,7 +15,7 @@ const part1 = () => {
     ]
       .map(([x, y]) => ({
         position: [x, y],
-        cost: input[y]?.[x],
+        cost: map[y]?.[x],
       }))
       .filter((x) => x.cost);
   };
@@ -23,8 +24,48 @@ const part1 = () => {
 };
 
 const part2 = () => {
-  return 0;
+  const increase = (riskLevel, add) => {
+    let result = riskLevel + add;
+    while (result > 9) {
+      result -= 9;
+    }
+    return result;
+  };
+
+  let map = input.map((line) => [
+    ...line,
+    ...line.map((x) => increase(x, 1)),
+    ...line.map((x) => increase(x, 2)),
+    ...line.map((x) => increase(x, 3)),
+    ...line.map((x) => increase(x, 4)),
+  ]);
+
+  map = [
+    ...map,
+    ...map.map((line) => line.map((x) => increase(x, 1))),
+    ...map.map((line) => line.map((x) => increase(x, 2))),
+    ...map.map((line) => line.map((x) => increase(x, 3))),
+    ...map.map((line) => line.map((x) => increase(x, 4))),
+  ];
+
+  const bottomRight = [map[0].length - 1, map.length - 1];
+
+  const getNeighbors = ([x, y]) => {
+    return [
+      [x, y - 1], // up
+      [x + 1, y], // right
+      [x, y + 1], // down
+      [x - 1, y], // left
+    ]
+      .map(([x, y]) => ({
+        position: [x, y],
+        cost: map[y]?.[x],
+      }))
+      .filter((x) => x.cost);
+  };
+
+  return astar([0, 0], bottomRight, getNeighbors);
 };
 
 console.log("#1:", part1()); // 527
-// console.log("#2:", part2()); // 0
+console.log("#2:", part2()); // 2887
