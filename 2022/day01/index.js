2c1af0a64031cf98c03e3f1f7132a14604da1e3f
emilpalsson <emil@emilpalsson.se>
2022-12-02 08:58:36 +0100

    22:01 - Refactor

--- 2022/day01/index.js
+++ 2022/day01/index.js
@@ -2,14 +2,15 @@ const { getInput, sum } = require("../../utils");
 const input = getInput()
   .replaceAll("\r", "")
   .split("\n\n")
-  .map((x) => sum(x.split("\n").map(Number)));
+  .map((x) => sum(x.split("\n").map(Number)))
+  .sort((a, b) => b - a);
 
 const part1 = () => {
-  return Math.max(...input);
+  return input[0];
 };
 
 const part2 = () => {
-  return sum(input.sort((a, b) => b - a).slice(0, 3));
+  return sum(input.slice(0, 3));
 };
 
 console.log("#1:", part1()); // 71300
