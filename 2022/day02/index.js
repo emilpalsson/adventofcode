ffea70f6c7ece22778f84c533977a1d22af99aed
emilpalsson <emil@emilpalsson.se>
2022-12-03 21:08:45 +0100

    22:02 - First working part 2 + refactor

--- 2022/day02/index.js
+++ 2022/day02/index.js
@@ -1,51 +1,48 @@
 const { getInput, sum } = require("../../utils");
-const input = getInput(true).map((x) => x.split(" "));
+const input = getInput(true);
 
-const OponentShape = {
-  Rock: "A",
-  Paper: "B",
-  Scissors: "C",
-};
-
-const YourShape = {
-  Rock: "X",
-  Paper: "Y",
-  Scissors: "Z",
-};
-
-const ShapeScore = {
-  [YourShape.Rock]: 1,
-  [YourShape.Paper]: 2,
-  [YourShape.Scissors]: 3,
-};
-
-const Outcome = {
+const OutcomeScore = {
   Win: 6,
   Draw: 3,
   Loss: 0,
 };
-
-const getOutcome = (oponentPlay, yourPlay) => {
-  if (yourPlay === YourShape.Rock && oponentPlay === OponentShape.Rock) return Outcome.Draw;
-  if (yourPlay === YourShape.Rock && oponentPlay === OponentShape.Paper) return Outcome.Loss;
-  if (yourPlay === YourShape.Rock && oponentPlay === OponentShape.Scissors) return Outcome.Win;
-
-  if (yourPlay === YourShape.Paper && oponentPlay === OponentShape.Rock) return Outcome.Win;
-  if (yourPlay === YourShape.Paper && oponentPlay === OponentShape.Paper) return Outcome.Draw;
-  if (yourPlay === YourShape.Paper && oponentPlay === OponentShape.Scissors) return Outcome.Loss;
-
-  if (yourPlay === YourShape.Scissors && oponentPlay === OponentShape.Rock) return Outcome.Loss;
-  if (yourPlay === YourShape.Scissors && oponentPlay === OponentShape.Paper) return Outcome.Win;
-  if (yourPlay === YourShape.Scissors && oponentPlay === OponentShape.Scissors) return Outcome.Draw;
+const ShapeScore = {
+  Rock: 1,
+  Paper: 2,
+  Scissors: 3,
 };
 
 const part1 = () => {
-  return sum(input.map(([oponent, you]) => getOutcome(oponent, you) + ShapeScore[you]));
+  const ScoreMap = {
+    "A X": OutcomeScore.Draw + ShapeScore.Rock,
+    "A Y": OutcomeScore.Win + ShapeScore.Paper,
+    "A Z": OutcomeScore.Loss + ShapeScore.Scissors,
+    "B X": OutcomeScore.Loss + ShapeScore.Rock,
+    "B Y": OutcomeScore.Draw + ShapeScore.Paper,
+    "B Z": OutcomeScore.Win + ShapeScore.Scissors,
+    "C X": OutcomeScore.Win + ShapeScore.Rock,
+    "C Y": OutcomeScore.Loss + ShapeScore.Paper,
+    "C Z": OutcomeScore.Draw + ShapeScore.Scissors,
+  };
+
+  return sum(input.map((x) => ScoreMap[x]));
 };
 
 const part2 = () => {
-  return sum(input.slice(0, 3));
+  const ScoreMap = {
+    "A X": OutcomeScore.Loss + ShapeScore.Scissors,
+    "A Y": OutcomeScore.Draw + ShapeScore.Rock,
+    "A Z": OutcomeScore.Win + ShapeScore.Paper,
+    "B X": OutcomeScore.Loss + ShapeScore.Rock,
+    "B Y": OutcomeScore.Draw + ShapeScore.Paper,
+    "B Z": OutcomeScore.Win + ShapeScore.Scissors,
+    "C X": OutcomeScore.Loss + ShapeScore.Paper,
+    "C Y": OutcomeScore.Draw + ShapeScore.Scissors,
+    "C Z": OutcomeScore.Win + ShapeScore.Rock,
+  };
+
+  return sum(input.map((x) => ScoreMap[x]));
 };
 
 console.log("#1:", part1()); // 8933
-// console.log("#2:", part2());
+console.log("#2:", part2()); // 11998
