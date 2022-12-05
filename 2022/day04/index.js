77af9fe14481b14c37d9bcded633f99703cd2921
emilpalsson <emil@emilpalsson.se>
2022-12-05 22:59:13 +0100

    22:04 - First working part 2

--- 2022/day04/index.js
+++ 2022/day04/index.js
@@ -3,17 +3,25 @@ const input = getInput(true).map((line) =>
   line.split(",").map((sections) => sections.split("-").map(Number))
 );
 
-const isFullyOverlapping = (sections1, sections2) => {
-  return sections1[0] <= sections2[0] && sections1[1] >= sections2[1];
-};
-
 const part1 = () => {
+  const isFullyOverlapping = (sections1, sections2) => {
+    return sections1[0] <= sections2[0] && sections1[1] >= sections2[1];
+  };
+
   return input.filter(([elf1, elf2]) => {
     return isFullyOverlapping(elf1, elf2) || isFullyOverlapping(elf2, elf1);
   }).length;
 };
 
-const part2 = () => {};
+const part2 = () => {
+  const isPartiallyOverlapping = (sections1, sections2) => {
+    return sections1[0] <= sections2[1] && sections1[1] >= sections2[0];
+  };
+
+  return input.filter(([elf1, elf2]) => {
+    return isPartiallyOverlapping(elf1, elf2) || isPartiallyOverlapping(elf2, elf1);
+  }).length;
+};
 
 console.log("#1:", part1()); // 511
-// console.log("#2:", part2());
+console.log("#2:", part2()); // 821
