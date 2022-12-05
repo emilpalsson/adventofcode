a25e91f5e4e113abfe94b9bfa6091a61c08e9c74
emilpalsson <emil@emilpalsson.se>
2022-12-05 20:44:54 +0100

    22:03 - First working part 2

--- 2022/day03/index.js
+++ 2022/day03/index.js
@@ -1,5 +1,5 @@
 const { getInput, sum } = require("../../utils");
-const input = getInput(true);
+const input = getInput(true).map((x) => x.split(""));
 
 const getItemPriority = (item) => {
   const code = item.charCodeAt(0);
@@ -9,19 +9,29 @@ const getItemPriority = (item) => {
 
 const part1 = () => {
   const priorities = input.map((rucksack) => {
-    const compartment1 = rucksack.slice(0, rucksack.length / 2).split("");
-    const compartment2 = rucksack.slice(rucksack.length / 2).split("");
+    const compartment1 = rucksack.slice(0, rucksack.length / 2);
+    const compartment2 = rucksack.slice(rucksack.length / 2);
 
-    const failedItemType = new Set(compartment1.filter((a) => compartment2.includes(a)))
-      .values()
-      .next().value;
+    const failedItemType = compartment1.find((a) => compartment2.includes(a));
     return getItemPriority(failedItemType);
   });
 
   return sum(priorities);
 };
 
-const part2 = () => {};
+const part2 = () => {
+  const groups = [];
+  for (let i = 0; i < input.length; i += 3) {
+    groups.push([input[i], input[i + 1], input[i + 2]]);
+  }
+
+  const priorities = groups.map((group) => {
+    const badge = group[0].find((x) => group[1].includes(x) && group[2].includes(x));
+    return getItemPriority(badge);
+  });
+
+  return sum(priorities);
+};
 
 console.log("#1:", part1()); // 7826
-// console.log("#2:", part2());
+console.log("#2:", part2()); // 2577
