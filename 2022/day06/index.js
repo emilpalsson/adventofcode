80c6ea59000bfd0acb464f7936dc99e62a8872d2
emilpalsson <emil@emilpalsson.se>
2022-12-06 16:52:34 +0100

    22:06 - First working part 2

--- 2022/day06/index.js
+++ 2022/day06/index.js
@@ -16,7 +16,20 @@ const part1 = () => {
   return index;
 };
 
-const part2 = () => {};
+const part2 = () => {
+  let index = 14;
+  while (index < input.length) {
+    const sequence = input.slice(index - 14, index);
+
+    if (new Set(sequence.split("")).size === 14) {
+      break;
+    }
+
+    index++;
+  }
+
+  return index;
+};
 
 console.log("#1:", part1()); // 1175
-// console.log("#2:", part2());
+console.log("#2:", part2());
