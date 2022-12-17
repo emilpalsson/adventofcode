06197c982cb022e42cd15b47c6bc0392ce2575f3
emilpalsson <emil@emilpalsson.se>
2022-12-17 22:11:13 +0100

    22:09 - First working part 2

--- 2022/day09/index.js
+++ 2022/day09/index.js
@@ -42,7 +42,49 @@ const part1 = () => {
   return tailVisits.size;
 };
 
-const part2 = () => {};
+const part2 = () => {
+  const knots = [];
+  for (let i = 0; i < 10; i++) knots.push({ x: 0, y: 0 });
+  const head = knots[0];
+  const tail = knots.at(-1);
+  const tailVisits = new Set([posId(tail)]);
+
+  const moveKnotVertically = (knot, step, parentX) => {
+    knot.y += step;
+    if (parentX > knot.x) knot.x++;
+    if (parentX < knot.x) knot.x--;
+  };
+  const moveKnotHorizontally = (knot, step, parentY) => {
+    knot.x += step;
+    if (parentY > knot.y) knot.y++;
+    if (parentY < knot.y) knot.y--;
+  };
+
+  input.forEach((operation) => {
+    for (let s = 0; s < operation.steps; s++) {
+      // prettier-ignore
+      switch (operation.direction) {
+        case 'U': head.y--; break;
+        case 'R': head.x++; break;
+        case 'D': head.y++; break;
+        case 'L': head.x--; break;
+        default: throw new Error("wtf direction", direction);
+      }
+
+      for (let i = 1; i < knots.length; i++) {
+        const knot = knots[i];
+        const parentKnot = knots[i - 1];
+        if (knot.x < parentKnot.x - 1) moveKnotHorizontally(knot, 1, parentKnot.y);
+        if (knot.x > parentKnot.x + 1) moveKnotHorizontally(knot, -1, parentKnot.y);
+        if (knot.y < parentKnot.y - 1) moveKnotVertically(knot, 1, parentKnot.x);
+        if (knot.y > parentKnot.y + 1) moveKnotVertically(knot, -1, parentKnot.x);
+      }
+      tailVisits.add(posId(tail));
+    }
+  });
+
+  return tailVisits.size;
+};
 
 console.log("#1:", part1()); // 6391
-// console.log("#2:", part2());
+console.log("#2:", part2()); // 2593
