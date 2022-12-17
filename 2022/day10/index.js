291b841c3d6358d4e833e1abe01855d82593e67a
emilpalsson <emil@emilpalsson.se>
2022-12-17 23:07:13 +0100

    22:10 - First working part 2

--- 2022/day10/index.js
+++ 2022/day10/index.js
@@ -26,7 +26,37 @@ const part1 = () => {
   return answer;
 };
 
-const part2 = () => {};
+const part2 = () => {
+  let register = 1;
+  let cycle = 0;
+
+  let crtScreen = "";
+  let crtLine = "";
+
+  const tick = () => {
+    const crtPos = cycle % 40;
+    crtLine += crtPos >= register - 1 && crtPos <= register + 1 ? "#" : " ";
+
+    if (crtPos === 39) {
+      crtScreen += "\n" + crtLine;
+      crtLine = "";
+    }
+
+    cycle++;
+  };
+
+  input.forEach((instruction) => {
+    if (instruction === "noop") {
+      tick();
+    } else if (instruction.startsWith("addx ")) {
+      tick();
+      tick();
+      register += Number(instruction.slice(5));
+    }
+  });
+
+  return crtScreen;
+};
 
 console.log("#1:", part1()); // 17020
-// console.log("#2:", part2());
+console.log("#2:", part2()); // RLEZFLGE
